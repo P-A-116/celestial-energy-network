2eda7999673b804fb412c4c90c326f728bc59f71
P-A-116 <rotateme314@gmail.com>
2024-12-03 14:31:01 +0200

    51

--- main.js
+++ main.js
@@ -94,7 +94,6 @@ document.addEventListener("DOMContentLoaded", () => {
         // Create table
         const table = document.createElement("table");
         table.className = "min-w-full bg-white border border-gray-300 rounded-lg shadow-md mb-6";
-		table.style.margin = "0 auto";
 
         // Create table header
         const thead = document.createElement("thead");
